3c8b8f7c9dce4db0e7b630ffd8c1e5199a9ee1b0
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-02-26 12:10:47 +0100

    add test on TransferReceived model

--- tests/Model/TransferReceivedTest.php
+++ tests/Model/TransferReceivedTest.php
@@ -7,10 +7,10 @@ use PHPUnit\Framework\TestCase;
 
 final class TransferReceivedTest extends TestCase
 {
-    public function testCreate()
+    public function testCreateModel1()
     {
         $test1 = "VIR RECU 9936053227134".
-            "\nDE: MLLE SOPHIE VERMOTE" .
+            "\nDE: MLLE GEGE RALDINE" .
             "\nMOTIF: complement";
 
         preg_match(TransferReceived::PATTERN, $test1, $matches);
@@ -28,13 +28,49 @@ final class TransferReceivedTest extends TestCase
         );
 
         $this->assertEquals(
-            'MLLE SOPHIE VERMOTE',
+            'MLLE GEGE RALDINE',
             $transferReceived->getFrom()
         );
 
         $this->assertEquals(
             'complement',
             $transferReceived->getReason()
+        );
+    }
+    
+    public function testCreateModel2()
+    {
+        $model2 = "VIREMENT RECU".
+                    "\nDE: MLLE GEGE RALDINE".
+                    "\nMOTIF: Vie quotidienne".
+                    "\nREF: 1000117069699";
+
+        preg_match(TransferReceived::PATTERN, $model2, $matches);
+        $transferReceived = TransferReceived::create($matches);
+
+        $this->assertInstanceOf(
+            TransferReceived::class,
+            $transferReceived
+        );
+
+        $this->assertEquals(
+            '',
+            $transferReceived->getNumber()
+        );
+
+        $this->assertEquals(
+            'MLLE GEGE RALDINE',
+            $transferReceived->getFrom()
+        );
+
+        $this->assertEquals(
+            'Vie quotidienne',
+            $transferReceived->getReason()
+        );
+
+        $this->assertEquals(
+            '1000117069699',
+            $transferReceived->getRef()
         );
     }
 }
